53565f0d066cbed3cabd9f0a474b5da3e76c384e
Dyego H <dyegohbb@gmail.com>
2025-05-17 13:06:04 -0300

    :tada: Inicialização do projeto

---
+++ src/main/java/com/contactfy/ContactfyApiApplication.java
@@ -0,0 +1,13 @@
+package com.contactfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactfyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactfyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contactfy/ContactfyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactfyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
